ab8783d7dc5578d3f41f7683158e8906057e3c7e
Kalzangdorjelama <kalzanglama77@gmail.com>
2024-11-30 22:56:48 +0545

    add user field

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
-  
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
 });
 
-export { 
-  registerUser, 
-};
+export { registerUser };
