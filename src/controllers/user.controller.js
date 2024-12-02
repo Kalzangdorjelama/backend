c73feabdd371ccf6253febcbf3ca6cce0b5d9d95
Kalzangdorjelama <kalzanglama77@gmail.com>
2024-12-02 13:14:11 +0545

    fix all register controller bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,15 +9,18 @@ const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
   // console.log("email: ", email);
 
+  // console.log(req.body);
+  // console.log(req.files);
+
   // Validation
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
-    throw new ApiError(400, "fullname is required");
+    throw new ApiError(400, "All fields are required");
   }
 
   // check if user already exist in the database
-  const existUser = User.findOne({
+  const existUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   // const existUser = User.findOne({username}) // for checking only the username
@@ -27,7 +30,15 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check for image , check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path; // TypeError: Cannot read properties of undefined (reading &#39;0&#39;) yo aauxa if the coverImage is not send but below code can fixed this type of error so better use below code  just try both code one by one in your VsCode 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
@@ -42,17 +53,17 @@ const registerUser = asyncHandler(async (req, res) => {
   // create user obj - create entry in database
   const user = await User.create({
     fullName,
-    username: username.tolowerCase(),
     email,
     password,
+    username: username.toLowerCase(),
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
   });
 
   // remove password and refresh token field from response
-  const createdUser = await user
-    .findById(user._id)
-    .select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
   // check for user creation
   if (!createdUser) {
