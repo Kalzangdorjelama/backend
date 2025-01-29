71de000319e0c4f36d3ed8deaf7728a986f34fbe
Kalzangdorjelama <kalzanglama77@gmail.com>
2025-01-30 00:18:08 +0545

    Refactor tweet controller; add new methods for user tweets, update, and delete functionality with improved code organization and clarity

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,34 +1,34 @@
-import mongoose, { isValidObjectId } from "mongoose";
-import { Tweet } from "../models/tweet.model.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose, { isValidObjectId } from "mongoose"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-  const { content } = req.body;
-
-  // console.log(req.body)
-
-  // console.log(content);
-
-  if (!content || !content.trim()) {
-    throw new ApiError(400, "Tweet cannot be empty");
-  }
-
-  const tweet = await Tweet.create({
-    content,
-    owner: req.user?._id,
-  });
-
-  console.log(tweet);
-
-  if (!tweet) {
-    throw new ApiError(500, "Error while adding tweets");
-  }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, tweet, "Tweet created successfully"));
+  // get content from req.body
+  // check if content is empty
+  // create tweet
+  // return response
+  
+  
 });
 
-export { createTweet };
+const getUserTweets = asyncHandler(async (req, res) => {
+   
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+  //TODO: update tweet
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  //TODO: delete tweet
+})
+
+export {
+  createTweet,
+  getUserTweets,
+  updateTweet,
+  deleteTweet
+}
